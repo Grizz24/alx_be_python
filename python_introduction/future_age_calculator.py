84d785761b3ae48e62fc28fb9ad5eb6dc1347706
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-07-10 15:35:16 +0200

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,5 +1,8 @@
-age = int(input(" How old are you"))
+print("How old are you?")
+age = input()
 
-sum = age + 27
+age = int(age)
 
-print(f"In 2050, you will be {sum} years old.")
+future_age = age + 27
+
+print(f"In 2050, you will be {future_age} years old.")
